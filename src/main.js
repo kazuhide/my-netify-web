07115b695dffc49918b4bcd8dc4147a9cad6d169
Kazuhide Yamamoto <kazuhide.yamamoto@bitmovin.com>
2020-04-11 17:18:52 +0900

    added a simple video tag

---
+++ src/main.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    var player;
+
+    var conf = {
+        key: 'fa2e1e52-eef5-4d53-ad4d-0b01b906e950'
+    };
+    var source = {
+        dash: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+        hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+        progressive: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
+        poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
+    };
+
+    player = new bitmovin.player.Player(document.getElementById('player'), conf);
+
+    player.load(source).then(function () {
+        console.log('Successfully loaded source'); // Success!
+    }, function () {
+        console.log('Error while loading source'); // Error!
+    });
+}
