6d5c80e629bf647a691671822ab76a89da503daf
Kazuhide Yamamoto <34905701+kazuhide@users.noreply.github.com>
2020-07-11 23:09:16 +0900

    change jpg to webp

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         dash: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
         hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
         progressive: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
-        poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
+        poster: './img/poster.webp'
     };
     player = new bitmovin.player.core.Player(document.getElementById('player'), conf);
 
